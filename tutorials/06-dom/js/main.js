93f2e13fa6777ee1601fd12e887ae5e2a9d967be
andreatostado <andrea.tostado@sjsu.edu>
2026-09-25 23:20:31 -0700

    tut 9

---
+++ tutorials/06-dom/js/main.js
@@ -0,0 +1,47 @@
+// console.log("note")
+
+
+// select html elements 
+const header = document.querySelector("#header");
+const changeHeaderButton = document.querySelector("#change-header-button");
+const changeThemeButton = document.querySelector("#change-theme-button");
+const img1 = document.querySelector("#img1");
+const img2 = document.querySelector("#img2");
+const img3 = document.querySelector("#img3");
+
+// change header with button click 
+changeHeaderButton.addEventListener("click",()=>{
+    header.innerHTML = "BOOM!!";
+})
+
+
+
+
+// toggele color theme 
+
+//create function for changing button text
+function changeButtonText(){
+    if (document.body.classList.contains("dark")) {
+        changeThemeButton.textContent = "switch to light theme";
+    } else {
+        changeThemeButton.textContent = "Switch to Dark Theme"
+    }
+}
+
+// click event on button
+changeThemeButton.addEventListener("click", ()=>{
+    //add dark class to body
+    document.body.classList.toggle("dark");
+    changeButtonText();
+})
+
+// toggle image visibility
+
+img1.addEventListener("click", () => {
+      img2.classList.remove("hidden");
+     
+})
+img2.addEventListener("click", () => {
+      img3.classList.remove("hidden");
+     
+})
